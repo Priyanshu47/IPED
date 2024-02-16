8c51b59e334f38d27224d61d853c4a1f0e9e416b
p4iyanshu7 <pv69290@gmail.com>
2024-02-17 05:14:19 +0530

    api

--- src/Assest/api/Authentication/Login.js
+++ src/Assest/api/Authentication/Login.js
@@ -3,14 +3,14 @@ import axios from 'axios';
 // Function for api connection
 export default async function loginApi(data) {
     try {
-        console.log("here")
+        
         // const loginAPI = API_CONFIG.API_BASE_URL + API_CONFIG.LOGIN_API;
         // const apiTocken = API_CONFIG.API_TOKEN;
         const response = await axios.post(`http://localhost:8000/auth/login`, data);
-        // return response?.data
-        console.log("abc",response)
+       return response?.data
+       
     }
     catch {
-
+            
     }
 }
--- src/Pages/Authentication/Login.js
+++ src/Pages/Authentication/Login.js
@@ -3,16 +3,19 @@ import React, { useState } from "react";
 import DefaultENG from "../../Assest/Languages/Defaulttext.json"
 import Nav from '../../components/navbar'
 import { useEffect} from "react";
+import loginApi from "@/Assest/api/Authentication/Login";
 import { useRouter } from "next/navigation";
-// import axios from "axios";
-import Link from "next/link";
-// import { toast } from "react-toastify";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
-  const [email, setEmail] = useState(''); 
+    const [email, setEmail] = useState(''); 
     const [password, setPassword] = useState(''); 
     const [errors, setErrors] = useState({}); 
     const [isFormValid, setIsFormValid] = useState(false); 
+    const router = useRouter();
+    const showToastMessage = () => { };
+   
 
     useEffect(() => { 
       validateForm(); 
@@ -21,13 +24,13 @@ const Login = () => {
   const validateForm = () => { 
       let errors = {}; 
       if (!email) { 
-          errors.email = DefaultENG.Login_Page.emailReq; 
+         errors.email = ([]); 
       } else if (!/\S+@\S+\.\S+/.test(email)) { 
           errors.email = DefaultENG.Login_Page.emailInv; 
       } 
 
       if (!password) { 
-          errors.password = DefaultENG.Login_Page.passwordReq; 
+        errors.password = ([]); 
       } else if (password.length < 6) { 
           errors.password = DefaultENG.Login_Page.passwordInv; 
       } 
@@ -35,80 +38,80 @@ const Login = () => {
       setErrors(errors); 
       setIsFormValid(Object.keys(errors).length === 0); 
   }; 
-  // Submit
-  const handleSubmit = () => { 
-      if (isFormValid) { 
-        alert('Login successfully!'); 
+  
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Access the form elements through the event.target
+   
+   let logindata ={
+    email : event.target.email.value,
+    password : event.target.password.value
+   }
+    if (isFormValid) { 
+       const data = await loginApi(logindata) 
+       if (data?.code == 200) {
+        router.push("/dashboard");
+        alert("Login Succsfully")
+       }
+       else {
+        toast.error("Incorrect Email Id And Password");
+       }
       } else { 
-          alert('Form has errors. Please correct them.'); 
+        toast.warning("Input Field Empty");
+        // alert('Form has errors. Please correct them.'); 
       } 
-  }; 
-
-  // const router = useRouter();
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   e.preventDefault();
-  //   const payload = {
-  //     email,
-  //     password,
-  //   };
-
-  //   axios
-  //     .post(`http://localhost:3001/Login`, payload)
-  //     .then((res) => {
-  //       console.log(res.data);
-  //       toast.success(<div>Successfully Login</div>);
-
-  //       router.push("/");
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
-
+  
+  };
 
   return (
     <>
     <Nav/>
-      <div className="flex items-center justify-center min-h-[80vh] font-Mulish bg-white">
+<div className="flex items-center justify-center min-h-[80vh] font-Mulish bg-white">
+  <form onSubmit={handleSubmit} className="w-[430px] px-8 py-10 mx-auto bg-white border rounded-lg">
+    <p className="text-base text-custom-red">{DefaultENG.Login_Page.welcomemsg}</p>
+    <h3 className="text-2xl font-semibold">{DefaultENG.Login_Page.login}</h3>
+    <div className="my-5">
+      <label>{DefaultENG.Login_Page.email}</label>
+      <input
+        type="email"
+        className="border w-full py-2 px-2 rounded-md hover:border-indigo-600"
+        placeholder="johndoe@gmail.com"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        name="email"
+      />
+      {errors.email && <p style={styles.error}>{errors.email}</p>} 
+    </div>
+    <div className="my-5">
+      <label>{DefaultENG.Login_Page.password}</label>
+      <input
+        type="password"
+        className="border w-full py-2 px-2 text-neutral-500 rounded-md hover:border-indigo-600"
+        placeholder="Enter Password"
+        value={password} 
+        onChange={(e) => setPassword(e.target.value)} 
+        name="password"
+      />
+      {errors.password && <p style={styles.error}>{errors.password}</p>}  
+    </div>
+    <button
+    
+      onClick={showToastMessage}
+      type="submit"
+      className="w-full h-12 px-3.5 py-2 bg-custom-blue rounded-3xl justify-center items-center gap-2.5 inline-flex text-white text-base font-bold font-Mulish leading-none"
+    >
+      <ToastContainer />
       
-        <div className="w-[430px] px-8 py-10 mx-auto bg-white border rounded-lg">
-          <p className="text-base text-custom-red">{DefaultENG.Login_Page.welcomemsg}</p>
-          <h3 className="text-2xl font-semibold">{DefaultENG.Login_Page.login}</h3>
-          <div className="my-5">
-            <label>{DefaultENG.Login_Page.email}</label>
-            <input
-              type="email"
-              className="border w-full py-2 px-2 rounded-md hover:border-indigo-600"
-              placeholder="johndoe|"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {errors.email && <p style={styles.error}>{errors.email}</p>} 
-            </div>
-            <div className="my-5">
-            <label>{DefaultENG.Login_Page.password}</label>
-            <input
-              type="Password"
-              className="border w-full py-2 px-2 text-neutral-500 rounded-md hover:border-indigo-600"
-              placeholder="Enter Password"
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)} 
-            />
-             {errors.password && <p style={styles.error}>{errors.password}</p>}  
-            </div>
-
-            <Link href="/dashboard">
-            <button className="w-full h-12 px-3.5 py-2 bg-custom-blue rounded-3xl justify-center items-center gap-2.5 inline-flex text-white text-base font-bold font-Mulish leading-none"
-            onClick={handleSubmit}
-            >
-              {DefaultENG.Login_Page.loginbtn}
-            </button>
-            </Link>
-            <p className="text-center pt-5 text-custom-sky text-s font-normal font-Mulish underline">
-              {DefaultENG.Login_Page.cantaccess}
-            </p>
-        </div>
-      </div>
-    </>
+      {DefaultENG.Login_Page.loginbtn}
+    </button>
+    <p className="text-center pt-5 text-custom-sky text-s font-normal font-Mulish underline">
+      {DefaultENG.Login_Page.cantaccess}
+    </p>
+  </form>
+</div>
+</>
   );
 };
 const styles = { 
